6854fb5ab91adfb65bb9dea49afe26a9ba9a91b1
ZianTT <2508164094@qq.com>
2024-06-15 14:27:01 +0000

    serverless傻逼

---
+++ index.js
@@ -0,0 +1,38 @@
+var slide_result = require('./slide.js');
+var click_result = require('./click.js');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  	res.send('Hello World!')
+})
+
+app.use(express.json())
+app.post('/click', (req, res) => {
+	const positions = req.body.positions;
+  	const gt = req.body.gt;
+  	const challenge = req.body.challenge;
+  	const c = req.body.c;
+  	const s = req.body.s;
+  	const rt = req.body.rt;
+  	const dist = click_result(positions, gt, challenge, c, s, rt)
+  	res.send(dist);
+}
+)
+
+app.post('/slide', (req, res) => {
+	const dis = req.body.dis;
+	const gt = req.body.gt;
+  	const challenge = req.body.challenge;
+  	const c = req.body.c;
+  	const s = req.body.s;
+  	const rt = req.body.rt;
+  	const dist = slide_result(dis, gt, challenge, c, s, rt)
+  	res.send(dist);
+}
+)
+
+app.listen(port, () => {
+  	console.log(`Dingzhenma Server listening on port ${port}`)
+})
